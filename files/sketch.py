9b1a8bf84dc2b8360fe7eae208bd390f40c93189
ielson <danmascandrade@gmail.com>
2023-07-17 17:21:35 -0300

    added exception handling

---
+++ files/sketch.py
@@ -0,0 +1,9 @@
+data = open('./files/sketch.txt')
+for each_line in data:
+    try:
+        (role, line_spoken) = each_line.split(':', 1)
+        print(role, end='')
+        print(' said: ', end='')
+        print(line_spoken, end='')
+    except:
+        pass
