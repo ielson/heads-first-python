5659b20e8e74e65da6985f218d5002205d4711f6
ielson <danmascandrade@gmail.com>
2023-07-17 17:36:37 -0300

    added try block to read file

--- files/sketch.py
+++ files/sketch.py
@@ -1,9 +1,12 @@
-data = open('./files/sketch.txt')
-for each_line in data:
-    try:
-        (role, line_spoken) = each_line.split(':', 1)
-        print(role, end='')
-        print(' said: ', end='')
-        print(line_spoken, end='')
-    except:
-        pass
+try:
+    data = open('./files/sketch.txt')
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(':', 1)
+            print(role, end='')
+            print(' said: ', end='')
+            print(line_spoken, end='')
+        except ValueError:
+            pass
+except IOError:
+    print('file not found')
