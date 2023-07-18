1da2a8907fe5ce6e6cadb706c8916a1bf3fca19f
ielson <danmascandrade@gmail.com>
2023-07-18 16:10:32 -0300

    start chp5

---
+++ treinador/coach_data.py
@@ -0,0 +1,19 @@
+import pathlib
+
+current_directory = pathlib.Path(__file__)
+
+
+def get_txt_files(folder=current_directory):
+    parent_directory = folder.parent
+    print("current: " + str(parent_directory)) 
+    txt_list = list(pathlib.Path(parent_directory).glob('*.txt'))
+    print("files: " + str(txt_list))
+    return txt_list
+
+
+for file in get_txt_files():
+    with open(file) as athlete_file:
+        athlete_data = athlete_file.readline()
+        athlete_file = athlete_data.strip()
+        print(athlete_data)
+
