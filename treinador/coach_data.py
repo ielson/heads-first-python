966374835977c6cb1ba92bf0e4da7551136bd145
ielson <danmascandrade@gmail.com>
2023-07-19 19:45:48 -0300

    changed to a dict creation inside of a function, returning it (great idea)

--- treinador/coach_data.py
+++ treinador/coach_data.py
@@ -13,7 +13,8 @@ def get_data_from_file(file):
     try:
         with open(file) as f:
             data = f.readline()
-        return (data.strip().split(','))
+        temp = data.strip().split(',')
+        return {'name': temp.pop(0), 'dob': temp.pop(0), 'times': str(sorted(set(sanitize(t) for t in temp))[:3])}
     except IOError as err:
         print("File Error: " + err)
         return None
@@ -24,9 +25,4 @@ mikey = get_data_from_file('mikey2.txt')
 sarah = get_data_from_file('sarah2.txt')
 julie = get_data_from_file('julie2.txt')
 
-sarah_data = {}
-sarah_data['name'] = sarah.pop(0)
-sarah_data['dob'] = sarah.pop(0)
-sarah_data['times'] = sarah
-
-print(sarah_data['name']+ "'s fastest times are: " + str(sorted(set([sanitize(t) for t in sarah_data['times']]))[:3]))
+print(sarah['name'] + "'s fastest times are: " + sarah['times'])
