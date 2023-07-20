70b265164cc2d53086a8d205fb48e15b300f3e3e
ielson <danmascandrade@gmail.com>
2023-07-19 22:09:01 -0300

    inherited class list and can use its methods

--- treinador/coach_data.py
+++ treinador/coach_data.py
@@ -1,17 +1,12 @@
-class Athlete:
-    def __init__(self, name, dob, times) -> None:
+class AthleteList(list):
+    def __init__(self, name, dob=None, times=[]) -> None:
+        list.__init__([])
         self.name = name
         self.dob = dob
-        self.times = times
+        self.extend(times)
 
     def top3(self):
-        return (sorted(set([sanitize(t) for t in self.times]))[:3])
-
-    def add_time(self, time):
-        self.times.append(time)
-
-    def add_times(self, list_of_times):
-        self.times.extend(list_of_times)
+        return (sorted(set([sanitize(t) for t in self]))[:3])
 
 
 def sanitize(time_string):
@@ -30,7 +25,7 @@ def get_data_from_file(file):
         with open(file) as f:
             data = f.readline()
         temp = data.strip().split(',')
-        return Athlete(temp.pop(0), temp.pop(0), temp)
+        return AthleteList(temp.pop(0), temp.pop(0), temp)
     except IOError as err:
         print("File Error: " + err)
         return None
@@ -42,3 +37,5 @@ sarah = get_data_from_file('sarah2.txt')
 julie = get_data_from_file('julie2.txt')
 
 print(sarah.name + "'s fastest times are: " + str(sarah.top3()))
+print(mikey.name + "'s fastest times are: " + str(mikey.top3()))
+print(james.name + "'s fastest times are: " + str(james.top3()))
